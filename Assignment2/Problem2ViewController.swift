f790be6b9dc3bb185ca9f9c1b56917a833037104
Horacio Garza <horacio_garza@hks.harvard.edu>
2016-07-04 16:14:58 -0400

    Project Creation - Problem 1, up to step 11: success.

---
+++ Assignment2/Problem2ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Problem2ViewController.swift
+//  Assignment2
+//
+//  Created by Garza, Horacio on 7/4/16.
+//  Copyright © 2016 Horacio Garza. All rights reserved.
+//
+
+import UIKit
+
+class Problem2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "Problem 2"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Assignment2/Problem3ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Problem3ViewController.swift
+//  Assignment2
+//
+//  Created by Garza, Horacio on 7/4/16.
+//  Copyright © 2016 Horacio Garza. All rights reserved.
+//
+
+import UIKit
+
+class Problem3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "Problem 3"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
